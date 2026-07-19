9e7ce4bba265a335c9e45b9484e3c58d2da27168
Hairi21 <89725947+Hairi21@users.noreply.github.com>
2026-07-19 22:27:33 +0800

    Update app.js

--- app.js
+++ app.js
@@ -30,3 +30,33 @@ if (openModal && closeModal && productModal) {
     };
 
 }
+// SIDEBAR
+const menuButton = document.getElementById("menuButton");
+const sidebar = document.getElementById("sidebar");
+
+if (menuButton && sidebar) {
+    menuButton.onclick = function () {
+        sidebar.classList.toggle("active");
+    };
+}
+
+// MODAL TAMBAH PRODUK
+const openModal = document.getElementById("openModal");
+const closeModal = document.getElementById("closeModal");
+const productModal = document.getElementById("productModal");
+
+if (openModal && closeModal && productModal) {
+    openModal.onclick = function () {
+        productModal.classList.add("active");
+    };
+
+    closeModal.onclick = function () {
+        productModal.classList.remove("active");
+    };
+
+    window.onclick = function (e) {
+        if (e.target === productModal) {
+            productModal.classList.remove("active");
+        }
+    };
+}
