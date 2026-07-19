46439bdc345e3a0ee4c8a06dca054be3c459e584
Hairi21 <89725947+Hairi21@users.noreply.github.com>
2026-07-19 18:42:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+const menuButton = document.getElementById("menuButton");
 
+const sidebar = document.getElementById("sidebar");
+
+menuButton.onclick = function(){
+
+    sidebar.classList.toggle("active");
+
+};
